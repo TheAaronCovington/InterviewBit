7d3d20ec3f02342c6f403f9c06ba786bfc16e43e
Aaron Covington <TheAaronCovington@gmail.com>
2015-10-05 13:19:33 -0500

    Update 01_RotateArray.java

--- Arrays/01_RotateArray.java
+++ Arrays/01_RotateArray.java
@@ -3,6 +3,10 @@
                  B : 1
                  
    OUTPUT:  [2 3 4 5 6 1]
+   NOTE:  MOD is used to keep the array from overflowing.  The MOD operator has the following rules
+   1.) If X < Y then X % Y will always = X
+   2.) If X / Y is a whole number and not a fraction then X % Y will always = 0;
+   3.) If X / Y is a fraction then X % Y will be the remainder.
 */
 public class Solution {
 	public ArrayList<Integer> rotateArray(ArrayList<Integer> A, int B) {
